81107c5bdb9fce3f6e0622b57906bccd59bdc7b0
Jay <harsha.gontu@gmail.com>
2024-11-11 11:21:13 +0530

    react-redux

--- ecommerce/src/components/signUp/SignUp.jsx
+++ ecommerce/src/components/signUp/SignUp.jsx
@@ -1,5 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setErrors, updateFormData } from "../../features/sign-up/signUpSlice";
+import FormInput from "../formInput/FormInput";
+import CustomButton from "../customButton/CustomButton";
+import "./sign-up.scss";
 
 const SignUp = () => {
   const dispatch = useDispatch();
@@ -44,7 +47,51 @@ const SignUp = () => {
     }
   };
 
-  return <div>SignUp</div>;
+  return (
+    <div className="sign-up-container">
+      <h2>Not registered yet ? Create an account</h2>
+      <span>Sign up</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          name="displayName"
+          required
+          placeholder="Display Name"
+          onChange={handleChange}
+        />
+        {errors.displayName && (
+          <p className="error">{errors.displayName.message}</p>
+        )}
+
+        <FormInput type="email" name="email" required placeholder="Email" />
+        {errors.email && <p className="error">{errors.email.message}</p>}
+
+        <FormInput
+          type="password"
+          name="password"
+          required
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        {errors.password && <p className="error">{errors.password.message}</p>}
+
+        <FormInput
+          type="password"
+          name="confirmPassword"
+          required
+          placeholder="Confirm Password"
+          onChange={handleChange}
+        />
+        {errors.confirmPassword && (
+          <p className="error">{errors.confirmPassword.message}</p>
+        )}
+
+        <CustomButton type="submit" className="sign-in">
+          Sign Up
+        </CustomButton>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
